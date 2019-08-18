ac46538d722f11676f7edab5f4a386980a009960
Gareth Rushgrove <gareth@morethanseven.net>
2019-08-18 18:52:44 +0100

    Update latest schema versions
    I still need to catch up, but at least htis updates the latest release
    and master. More when I get a moment.

--- build.sh
+++ build.sh
@@ -1,26 +1,37 @@
 #!/bin/bash -xe
 
 # This script uses openapi2jsonschema to generate a set of JSON schemas for
-# the specified Kubernetes versions in three different flavours:
+# the specified Kubernetes versions in different flavours:
 #
 #   X.Y.Z - URL referenced based on the specified GitHub repository
 #   X.Y.Z-standalone - de-referenced schemas, more useful as standalone documents
+#   X.Y.Z-standalone-strict - de-referenced schemas, more useful as standalone documents, additionalProperties disallowe
 #   X.Y.Z-local - relative references, useful to avoid the network dependency
 
 REPO="garethr/openshift-json-schema"
 
-declare -a arr=(master
-                v3.6.0
-                v1.5.1
-                v1.5.0
-                )
+declare -a arr=(
+  master
+  v3.11.0
+)
+
+declare -a arr2=(
+  master
+  v3.6.0
+  v1.5.1
+  v1.5.0
+)
 
 for version in "${arr[@]}"
 do
     schema=https://raw.githubusercontent.com/openshift/origin/${version}/api/swagger-spec/openshift-openapi-spec.json
     prefix=https://raw.githubusercontent.com/${REPO}/master/${version}/_definitions.json
 
-    openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone --strict "${schema}"
+    openapi2jsonschema -o "${version}-standalone-strict" --expanded --kubernetes --stand-alone --strict "${schema}"
+    openapi2jsonschema -o "${version}-standalone" --expanded --kubernetes --stand-alone "${schema}"
+    openapi2jsonschema -o "${version}-local" --expanded --kubernetes "${schema}"
+    openapi2jsonschema -o "${version}" --expanded --kubernetes --prefix "${prefix}" "${schema}"
+    openapi2jsonschema -o "${version}-standalone-strict" --kubernetes --stand-alone --strict "${schema}"
     openapi2jsonschema -o "${version}-standalone" --kubernetes --stand-alone "${schema}"
     openapi2jsonschema -o "${version}-local" --kubernetes "${schema}"
     openapi2jsonschema -o "${version}" --kubernetes --prefix "${prefix}" "${schema}"
